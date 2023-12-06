3ef5d18d931072343ec0f2956275d41b2e9730c1
Jakub Martyński <69093918+XPenguin2002@users.noreply.github.com>
2023-12-06 19:39:22 +0100

    Update Zad2.java
    merge dwóch funkcji w jedną

--- Java/src/lab7/Zad2.java
+++ Java/src/lab7/Zad2.java
@@ -1,16 +1,15 @@
 package lab7;
 import java.io.*;
 public class Zad2 {
-    public void zapiszDoTxt(String nazwaPliku, String zawartosc) {
+    public void zapisz(String nazwaPliku, String zawartosc) {
         try (PrintWriter writer = new PrintWriter(new FileWriter(nazwaPliku))) {
             writer.println(zawartosc);
             System.out.println("Dane zostaly zapisane do pliku " + nazwaPliku);
         } catch (IOException e) {
             System.err.println("Blad podczas zapisu do pliku " + nazwaPliku);
-            e.printStackTrace();
         }
     }
-    public void odczytajZTxt(String nazwaPliku) {
+    public void odczytaj(String nazwaPliku) {
         try (BufferedReader reader = new BufferedReader(new FileReader(nazwaPliku))) {
             String linia;
             System.out.println("Odczyt danych z pliku " + nazwaPliku + ":");
@@ -19,35 +18,13 @@ public class Zad2 {
             }
         } catch (IOException e) {
             System.err.println("Blad podczas odczytu pliku " + nazwaPliku);
-            e.printStackTrace();
-        }
-    }
-    public void zapiszDoCsv(String nazwaPliku, String zawartosc) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(nazwaPliku))) {
-            writer.println(zawartosc);
-            System.out.println("Dane zostaly zapisane do pliku " + nazwaPliku);
-        } catch (IOException e) {
-            System.err.println("Blad podczas zapisu do pliku " + nazwaPliku);
-            e.printStackTrace();
-        }
-    }
-    public void odczytajZCsv(String nazwaPliku) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(nazwaPliku))) {
-            String linia;
-            System.out.println("Odczyt danych z pliku " + nazwaPliku + ":");
-            while ((linia = reader.readLine()) != null) {
-                System.out.println(linia);
-            }
-        } catch (IOException e) {
-            System.err.println("Blad podczas odczytu pliku " + nazwaPliku);
-            e.printStackTrace();
         }
     }
     public static void main(String[] args) {
         Zad2 plik = new Zad2();
-        plik.zapiszDoTxt("F:\\Java\\src\\lab7\\plik_txt.txt", "Elo benc txt 321.");
-        plik.odczytajZTxt("F:\\Java\\src\\lab7\\plik_txt.txt");
-        plik.zapiszDoCsv("F:\\Java\\src\\lab7\\plik_csv.csv", "Imi3,Nazwisko,Wiek\nJa,Jajowy,21\nBenc,Bencowy,22");
-        plik.odczytajZCsv("F:\\Java\\src\\lab7\\plik_csv.csv");
+        plik.zapisz("F:\\Java\\src\\lab7\\plik_txt.txt", "Elo benc txt 321.");
+        plik.odczytaj("F:\\Java\\src\\lab7\\plik_txt.txt");
+        plik.zapisz("F:\\Java\\src\\lab7\\plik_csv.csv", "Imi3,Nazwisko,Wiek\nJa,Jajowy,21\nBenc,Bencowy,22");
+        plik.odczytaj("F:\\Java\\src\\lab7\\plik_csv.csv");
     }
 }
