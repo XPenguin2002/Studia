8f814c3261c340351c8f7b1147b19c28a68616a2
Jakub Martyński <69093918+XPenguin2002@users.noreply.github.com>
2024-01-12 14:45:37 +0100

    Update Zad2.java
    poprawiona ścieżka plików

--- Java/src/lab7/Zad2.java
+++ Java/src/lab7/Zad2.java
@@ -22,9 +22,9 @@ public class Zad2 {
     }
     public static void main(String[] args) {
         Zad2 plik = new Zad2();
-        plik.zapisz("F:\\Java\\src\\lab7\\plik_txt.txt", "Elo benc txt 321.");
-        plik.odczytaj("F:\\Java\\src\\lab7\\plik_txt.txt");
-        plik.zapisz("F:\\Java\\src\\lab7\\plik_csv.csv", "Imie,Nazwisko,Wiek\nJa,Jajowy,21\nBenc,Bencowy,22");
-        plik.odczytaj("F:\\Java\\src\\lab7\\plik_csv.csv");
+        plik.zapisz("src\\lab7\\plik_txt.txt", "Elo benc txt 321.");
+        plik.odczytaj("src\\lab7\\plik_txt.txt");
+        plik.zapisz("src\\lab7\\plik_csv.csv", "Imie,Nazwisko,Wiek\nJa,Jajowy,21\nBenc,Bencowy,22");
+        plik.odczytaj("src\\lab7\\plik_csv.csv");
     }
 }
